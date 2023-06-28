b6dc8ad1418bf2739f52a18cc8140f81d1872953
kjrzxu <119322320+kjrzxu@users.noreply.github.com>
2023-06-28 17:11:49 +0800

    更新 diy-lede.sh

--- patch/diy-lede.sh
+++ patch/diy-lede.sh
@@ -6,6 +6,8 @@ sed -i '/^#.*helloworld/s/^#//' openwrt/feeds.conf.default
 #修改feeds.conf.default,在helloworld字段行后一行添加我的库package
 sed -i '/helloworld/a src-git package https://github.com/kjrzxu/package.git' openwrt/feeds.conf.default
 
+sed -i '/helloworld/a src-git openclash https://github.com/vernesong/OpenClash.git' openwrt/feeds.conf.default
+
 #修改默认插件
 #sed -i 's/luci-app-filetransfer //' openwrt/include/target.mk
 #sed -i '/ca-certificates/s/$/ \\/' openwrt/include/target.mk
