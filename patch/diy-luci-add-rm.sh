b7a6fe7597222408e937df8b6e50594c400e1486
kjrzxu <xhb1011@163.com>
2023-06-28 10:32:05 +0800

    add more files

--- patch/diy-luci-add-rm.sh
+++ patch/diy-luci-add-rm.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# 替换update_cloudflare_com_v4.sh
-rm -rf openwrt/feeds/packages/net/ddns-scripts/files/update_cloudflare_com_v4.sh
-cp files/update_cloudflare_com_v4.sh openwrt/feeds/packages/net/ddns-scripts/files
-
 # 替换原argon主题和argon-config主题设置
 rm -rf openwrt/feeds/luci/themes/luci-theme-argon
 rm -rf openwrt/feeds/luci/applications/luci-app-argon-config
@@ -12,6 +8,17 @@ cp -r files/luci-app-argon-config openwrt/feeds/luci/applications
 
 # 替换默认主题bootstrap为argon主题
 #sed -i 's/bootstrap/argon/ig' openwrt/feeds/luci/collections/luci/Makefile
+
+# 替换update_cloudflare_com_v4.sh
+rm -rf openwrt/feeds/packages/net/ddns-scripts/files/update_cloudflare_com_v4.sh
+cp files/update_cloudflare_com_v4.sh openwrt/feeds/packages/net/ddns-scripts/files
+
+#替换原cloudflared
+#sed -i '/cloudflared.init $/d' openwrt/feeds/packages/net/cloudflared/Makefile
+#sed -i '/init.d/d' openwrt/feeds/packages/net/cloudflared/Makefile
+#rm openwrt/feeds/packages/net/cloudflared/files/cloudflared.init
+rm -rf openwrt/feeds/packages/net/cloudflared
+cp -r files/cloudflared openwrt/feeds/packages/net
 
 #替换automount自动共享设置文件
 rm -rf openwrt/package/lean/automount/files/15-automount
@@ -26,8 +33,3 @@ sed -i 's/0666/0777/' openwrt/package/lean/autosamba/files/20-smb
 sed -i 's#sda#/mnt/sda1#' openwrt/feeds/packages/utils/hd-idle/files/hd-idle.config
 sed -i 's/10/5/' openwrt/feeds/packages/utils/hd-idle/files/hd-idle.config
 sed -i 's/0/1/' openwrt/feeds/packages/utils/hd-idle/files/hd-idle.config
-
-#sed -i '/cloudflared.init $/d' openwrt/feeds/packages/net/cloudflared/Makefile
-sed -i '/init.d/d' openwrt/feeds/packages/net/cloudflared/Makefile
-rm openwrt/feeds/packages/net/cloudflared/files/cloudflared.init
-
