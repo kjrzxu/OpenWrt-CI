6633ce002104e09a418a8afa88ec8e4220e2995d
kjrzxu <119322320+kjrzxu@users.noreply.github.com>
2023-09-03 12:17:02 +0800

    Update diy-luci-add-rm.sh

--- patch/diy-luci-add-rm.sh
+++ patch/diy-luci-add-rm.sh
@@ -20,7 +20,7 @@ sed -i 's/2023.2.2/2023.7.3/g' openwrt/feeds/packages/net/cloudflared/Makefile
 sed -i 's/b0abaff125d29c517894f6ea74dcc7044c92500670463595ba9ff4950a1d2fc2/772ddcb721f5b479192117d1156b1091505721aa81d6bab3de9577176b930191/g' openwrt/feeds/packages/net/cloudflared/Makefile
 sed -i '/init.d/d' openwrt/feeds/packages/net/cloudflared/Makefile
 rm -rf openwrt/feeds/packages/net/cloudflared/files/cloudflared.init
-sed -i '/config $(1)/d' openwrt/feeds/packages/net/cloudflared/Makefile
+sed -i '#config/cloudflared#d' openwrt/feeds/packages/net/cloudflared/Makefile
 rm -rf openwrt/feeds/packages/net/cloudflared/files/cloudflared.config
 #cp -r files/cloudflared.init openwrt/feeds/packages/net/cloudflared/files
 
