56c02640c4cc2955565b34da9c33288a357c0479
kjrzxu <119322320+kjrzxu@users.noreply.github.com>
2023-12-15 20:41:42 +0800

    Update diy-luci-add-rm.sh

--- patch/diy-luci-add-rm.sh
+++ patch/diy-luci-add-rm.sh
@@ -21,10 +21,9 @@ sed -i 's/b0abaff125d29c517894f6ea74dcc7044c92500670463595ba9ff4950a1d2fc2/2d2df
 #sed -i 's/2023.2.2/2023.7.3/g' openwrt/feeds/packages/net/cloudflared/Makefile
 #sed -i 's/b0abaff125d29c517894f6ea74dcc7044c92500670463595ba9ff4950a1d2fc2/772ddcb721f5b479192117d1156b1091505721aa81d6bab3de9577176b930191/g' openwrt/feeds/packages/net/cloudflared/Makefile
 sed -i '/init.d/d' openwrt/feeds/packages/net/cloudflared/Makefile
-rm -rf openwrt/feeds/packages/net/cloudflared/files/cloudflared.init
 sed -i '/cloudflared.config $(1)/d' openwrt/feeds/packages/net/cloudflared/Makefile
+rm -rf openwrt/feeds/packages/net/cloudflared/files/cloudflared.init
 rm -rf openwrt/feeds/packages/net/cloudflared/files/cloudflared.config
-#cp -r files/cloudflared.init openwrt/feeds/packages/net/cloudflared/files
 
 #替换原msd_lite
 rm -rf openwrt/feeds/packages/net/msd_lite
